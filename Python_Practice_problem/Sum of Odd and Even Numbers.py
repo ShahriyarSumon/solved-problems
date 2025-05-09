a9ee76cc379fc7b7a17330fd34ba6713c889b3d2
Md Sumon Mia <shahriyarsumon5@gmail.com>
2025-05-09 14:36:18 +0600

    Create Sum of Odd and Even Numbers.py

---
+++ Python_Practice_problem/Sum of Odd and Even Numbers.py
@@ -0,0 +1,12 @@
+numbers = [2, 5, 7, 8, 10, 11]
+even_sum = 0
+odd_sum = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_sum += num
+    else:
+        odd_sum += num
+
+print("Sum of even numbers:", even_sum)
+print("Sum of odd numbers:", odd_sum)
